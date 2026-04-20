702bbb121d59514f849e68e58084ac507fa4ac01
Edwin Orellana <edwinorellana@users.noreply.github.com>
2026-04-19 19:58:17 -0600

    Add files

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,8 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Override;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
@@ -22,6 +25,7 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+    #[Override]
     protected function casts(): array
     {
         return [
